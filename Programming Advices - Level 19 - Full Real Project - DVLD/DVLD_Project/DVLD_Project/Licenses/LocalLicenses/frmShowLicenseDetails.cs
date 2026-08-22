d8315eb98dfd5b42e2ab90d7efa1dbb78d6a0874
YounesJY <younjay01234@gmail.com>
2026-08-22 22:29:35 +0100

    Style: Clean up whitespace in frmShowLicenseDetails.cs
    - Added blank lines for better readability
    - Consistent spacing between methods

--- Programming Advices - Level 19 - Full Real Project - DVLD/DVLD_Project/DVLD_Project/Licenses/LocalLicenses/frmShowLicenseDetails.cs
+++ Programming Advices - Level 19 - Full Real Project - DVLD/DVLD_Project/DVLD_Project/Licenses/LocalLicenses/frmShowLicenseDetails.cs
@@ -15,16 +15,17 @@ namespace DVLD_Project.Licenses.LocalLicenses
     {
         private int _LocalDrivingApplicationID;
 
+
         public frmShowLicenseDetails(int localDrivingApplicationID)
         {
             InitializeComponent();
             this._LocalDrivingApplicationID = localDrivingApplicationID;
         }
-
         private void frmShowLicenseDetails_Load(object sender, EventArgs e)
         {
             ctrlDriverLicenseDetails.LoadDriverLicenseDetails(this._LocalDrivingApplicationID);
         }
+
 
         private void btnClose_Click(object sender, EventArgs e)
         {
