913aed94180a3a09dbf239900165526bdddb2759
YounesJY <younjay01234@gmail.com>
2026-08-31 07:40:37 +0100

    Add ctrlDriverLicenseDetailsWithFilter control
    - New control combining license details with person filter
    - Enables searching and viewing license details by person

---
+++ Programming Advices - Level 19 - Full Real Project - DVLD/DVLD_Project/DVLD_Project/Licenses/LocalLicenses/Controls/ctrlDriverLicenseDetailsWithFilter.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace DVLD_Project.Licenses.LocalLicenses.Controls
+{
+    partial class ctrlDriverLicenseDetailsWithFilter
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ctrlDriverLicenseDetails = new DVLD_Project.Licenses.Controls.ctrlDriverLicenseDetails();
+            this.gbFilter = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.txtLicenseID = new System.Windows.Forms.TextBox();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.gbFilter.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ctrlDriverLicenseDetails
+            // 
+            this.ctrlDriverLicenseDetails.Location = new System.Drawing.Point(3, 76);
+            this.ctrlDriverLicenseDetails.Name = "ctrlDriverLicenseDetails";
+            this.ctrlDriverLicenseDetails.Size = new System.Drawing.Size(816, 317);
+            this.ctrlDriverLicenseDetails.TabIndex = 0;
+            // 
+            // gbFilter
+            // 
+            this.gbFilter.Controls.Add(this.label1);
+            this.gbFilter.Controls.Add(this.btnFind);
+            this.gbFilter.Controls.Add(this.txtLicenseID);
+            this.gbFilter.Location = new System.Drawing.Point(3, 7);
+            this.gbFilter.Name = "gbFilter";
+            this.gbFilter.Size = new System.Drawing.Size(401, 63);
+            this.gbFilter.TabIndex = 18;
+            this.gbFilter.TabStop = false;
+            this.gbFilter.Text = "Filter";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "LicenseID:";
+            // 
+            // btnFind
+            // 
+            this.btnFind.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFind.Image = global::DVLD_Project.Properties.Resources.License_View_32;
+            this.btnFind.Location = new System.Drawing.Point(346, 18);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(44, 37);
+            this.btnFind.TabIndex = 18;
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // txtLicenseID
+            // 
+            this.txtLicenseID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLicenseID.Location = new System.Drawing.Point(119, 26);
+            this.txtLicenseID.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtLicenseID.Name = "txtLicenseID";
+            this.txtLicenseID.Size = new System.Drawing.Size(214, 20);
+            this.txtLicenseID.TabIndex = 17;
+            this.txtLicenseID.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtLicenseID_KeyPress);
+            this.txtLicenseID.Validating += new System.ComponentModel.CancelEventHandler(this.txtLicenseID_Validating);
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // ctrlDriverLicenseInfoWithFilter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gbFilter);
+            this.Controls.Add(this.ctrlDriverLicenseDetails);
+            this.Name = "ctrlDriverLicenseInfoWithFilter";
+            this.Size = new System.Drawing.Size(822, 396);
+            this.gbFilter.ResumeLayout(false);
+            this.gbFilter.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Licenses.Controls.ctrlDriverLicenseDetails ctrlDriverLicenseDetails;
+        private System.Windows.Forms.GroupBox gbFilter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.TextBox txtLicenseID;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+    }
+}
---
+++ Programming Advices - Level 19 - Full Real Project - DVLD/DVLD_Project/DVLD_Project/Licenses/LocalLicenses/Controls/ctrlDriverLicenseDetailsWithFilter.cs
@@ -0,0 +1,112 @@
+﻿using DVLD_Business;
+using DVLD_Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD_Project.Licenses.LocalLicenses.Controls
+{
+    public partial class ctrlDriverLicenseDetailsWithFilter : UserControl
+    {
+        public event Action<object, int> OnLicenseSelected;
+
+        private int _LicenseID = ValidationConstants.INVALID_ID;
+        private LicenseInfo _LicenseDetails = null;
+        public LicenseInfo SelectedLicenseDetails { get { return this._LicenseDetails; } }
+
+
+        public ctrlDriverLicenseDetailsWithFilter()
+        {
+            InitializeComponent();
+        }
+        public bool LoadDriverLicenseDetails(int LicenseID)
+        {
+            this._LicenseID = LicenseID;
+            if (!isValidLicense())
+                return false;
+
+            this.ctrlDriverLicenseDetails.LoadDriverLicenseDetails(this._LicenseID);
+            this.DisactiviteFilter();
+            return true;
+        }
+
+
+        private bool isValidLicense()
+        {
+            if (this._LicenseID <= 0)
+            {
+                MessageBox.Show($"Invalid LicenseID = {this._LicenseID}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            this._LicenseDetails = LicenseInfo.Find(this._LicenseID);
+            if (this._LicenseDetails == null)
+            {
+                MessageBox.Show($"Failed to load the license with ID = {this._LicenseID}. Please try again.",
+                                "Error",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+        private void txtLicenseID_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtLicenseID.Text.Trim()))
+            {
+                errorProvider.SetError(txtLicenseID, "This field is required!");
+                txtLicenseID.Focus();
+                e.Cancel = true;
+            }
+            else
+            {
+                errorProvider.SetError(txtLicenseID, null);
+                e.Cancel = false;
+            }
+        }
+        private void txtLicenseID_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+
+            if (e.KeyChar == (char)13)
+                btnFind.PerformClick();
+        }
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            if (!this.ValidateChildren())
+            {
+                //Here we dont continue becuase the form is not valid
+                MessageBox.Show("Some fileds are not valide!, put the mouse over the red icon(s) to see the erro", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtLicenseID.Focus();
+                return;
+            }
+
+            this._LicenseID = int.Parse(this.txtLicenseID.Text);
+            if (isValidLicense())
+            {
+                this.ctrlDriverLicenseDetails.LoadDriverLicenseDetails(this._LicenseID);
+                OnLicenseSelected?.Invoke(this, this._LicenseID);
+            }
+        }
+
+        public void ActivateFilter()
+        {
+            gbFilter.Enabled = true;
+        }
+        public void DisactiviteFilter()
+        {
+            gbFilter.Enabled = false;
+        }
+        public void FilterFocus()
+        {
+            txtLicenseID.Focus();
+        }
+    }
+}
