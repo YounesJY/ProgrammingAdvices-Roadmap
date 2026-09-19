4ce178389af7c32c44207ed2adc23602d25a13f5
YounesJY <younjay01234@gmail.com>
2026-09-19 11:02:36 +0100

    Add Custom Controls example (Lesson 82)
    - Demonstrates creating custom controls
    - Covers inheritance from Control and custom drawing

---
+++ Programming Advices - Level 20 - C# - Level 2/coursLessons/82 - Costume Controls/MyCustomTextBox.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class MyCustomTextBox : TextBox
+    {
+        public enum InputTypeEnum { TextInput, NumberInput }
+
+
+        public InputTypeEnum InputType { get; set; } = InputTypeEnum.TextInput;
+        public bool IsRequired { get; set; }
+
+
+        public MyCustomTextBox()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+        private bool IsNumeric()
+        {
+            string s = this.Text.Trim();
+            foreach (char c in s)
+            {
+                if (!char.IsDigit(c) && c != '.')
+                    return false;
+            }
+
+            return true;
+        }
+        public bool IsValid()
+        {
+            if (IsRequired)
+            {
+                if (this.Text.Trim().Length == 0)
+                    return false;
+            }
+
+            if (InputType == InputTypeEnum.NumberInput)
+                return IsNumeric();
+
+            return true;
+        }
+    }
+}
